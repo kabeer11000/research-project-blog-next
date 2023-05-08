2d9c2b0b9b45d0812e65f323257f8e7acd65f95d
kabeer11000 <kabeer@kabeersnetwork.tk>
2023-05-08 14:18:50 +0500

    rdy

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: `${process.env.KABEERS_NETWORK}/research/`,
-        permanent: true
-      }
-    ]
-  }
+    reactStrictMode: false,
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: `${process.env.KABEERS_NETWORK}/research/`,
+                permanent: true
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
